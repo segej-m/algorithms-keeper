7f271ab7216e837f6aef2207335667313899c37b
Dhruv Manilawala <dhruvmanila@gmail.com>
2021-06-10 22:55:53 +0530

    fix: only check if event is pull_request

--- algorithms_keeper/event/workflow.py
+++ algorithms_keeper/event/workflow.py
@@ -18,10 +18,7 @@ async def approve_workflow_run(
     """Approve a workflow run from a first-time contributor."""
     workflow_run = event.data["workflow_run"]
 
-    if (
-        workflow_run["event"] == "pull_request"
-        and workflow_run["conclusion"] == "action_required"
-    ):
+    if workflow_run["event"] == "pull_request":
         logger.info("Approving the workflow: %s", workflow_run["html_url"])
         await gh.post(
             workflow_run["url"] + "/approve", data={}, oauth_token=await gh.access_token
